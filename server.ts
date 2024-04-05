78a7a2a4ceb399d7f7e2db88f7dfb68f297b6fb2
jayant-vashisth <vashisthjayant.dev@gmail.com>
2024-04-05 21:23:32 +0530

    initial commit

---
+++ server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});
+
+app.listen(PORT, () => {
+  console.log("app listening");
+});
